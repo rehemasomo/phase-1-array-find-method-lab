52b93eb1d649f393e477fa372bdd9a99908eaeda
rehemasomo <rehemasomo30@gmail.com>
2024-03-29 22:37:10 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningRecord = record.find(game => game.result === "W"); // Using find() to search for a win
+    
+    // If a winning record is found, return the year of the win
+    // Otherwise, return undefined
+    return winningRecord ? winningRecord.year : undefined;
+  }
+  
+  
+  
